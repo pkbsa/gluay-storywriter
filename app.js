4b0d10c29985627272a6a81399ea6872b8d58cb0
pkbsa <siranut.poko@hotmail.com>
2023-10-10 19:45:07 +0700

    first commit

--- app.js
+++ app.js
@@ -6,28 +6,30 @@ var path = require("path");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 
+const FormData = require("form-data");
+const fetch = require("node-fetch");
+
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "bann-kt",
+  host: "23.106.53.137",
+  user: "gluaywwm_siranuta13",
+  password: "siranuta13",
+  database: "gluaywwm_gluaydatabase3",
 });
-  
+
 connection.connect((error) => {
-    if (error) console.log(error);
-    else console.log("MYSQL Connected...");
+  if (error) console.log(error);
+  else console.log("MYSQL Connected...");
 });
 
 var app = express();
 
 app.use(
-    session({
-      secret: "secret",
-      resave: true,
-      saveUninitialized: true,
-    })
-  );
-  
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,62 +41,92 @@ app.use(express.static(publicDirectory));
 
 app.set("view engine", "ejs");
 
-app.get("/", function (request, response) {
-    response.render("index");
+app.get("/storygluay", function (request, response) {
+  response.render("story-start");
 });
-app.get("/questions", function (request, response) {
-    response.render("qa");
+app.get("/storygluay-form", function (request, response) {
+  response.render("story-form");
 });
-// app.get("/results", function (request, response) {
-//     connection.query("SELECT * FROM catlist", function (error, results){
-//         if(error) throw error;
-//         response.render("catlist", { cats: results});
-//     })
-// });
-app.get("/submitted", function (request, response) {
-    response.render("done");
+app.get("/storygluay-questions", function (request, response) {
+  response.render("story-questions");
+});
 
+app.get("/storygluay-submitted", function (request, response) {
+  response.render("story-submitted");
 });
 
 
-// app.post("/addtips", function (request, response){
-//     let sampleFile;
-//     let uploadFile;
-
-//     console.log(request.body)
-
-//     if(!request.files || Object.keys(request.files).length === 0){
-//         connection.query("INSERT INTO tips SET ?",
-//         {
-//             title : request.body.title,
-//             text : request.body.text, 
-//             image : null,
-//         },
-//         (error, rows) => {
-//             response.redirect('/admin-index')
-//         });
-//     }else{
-//         sampleFile = request.files.sampleFile;
-//         uploadFile = __dirname + '/css/images/index/slide/' + sampleFile.name
-//         console.log(sampleFile)
-
-//         sampleFile.mv(uploadFile, function(error){
-//             if(error) return response.status(500).send(error)
-
-//             connection.query("INSERT INTO tips SET ?",
-//             {
-//                 title : request.body.title,
-//                 text : request.body.text,
-//                 image : sampleFile.name, 
-//             },
-//             (error, rows) => {
-//                 response.redirect('/admin-index')
-//             });
-//         });
-//     }
-// });
-
-
-app.listen(3000, function(){
-    console.log("Listening at Port 3000")
+app.get("/storygluay-diary", function (request, response) {
+  connection.query("SELECT * FROM gluay_story", function (error, results) {
+    if (error) throw error;
+    console.log(results)
+    response.render("story-diary", { drawing: results });
   });
+});
+
+app.post("/storygluay-submitstory", function (request, response) {
+  const currentDate = new Date();
+
+  const options = {
+    timeZone: "Asia/Bangkok", // Set the time zone to Thailand
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
+
+  const thaiDateTime = currentDate.toLocaleString("th-TH", options);
+
+  console.log(thaiDateTime);
+
+  console.log(request.body);
+
+  connection.query(
+    "INSERT INTO gluay_story SET ?",
+    {
+      title: request.body.title,
+      story: request.body.story,
+      author: request.body.author,
+      instagram: request.body.instagram,
+      uploaded_at: thaiDateTime,
+    },
+    (error, rows) => {
+      console.log(error);
+      const line_api = "lcbnB8JtsygvD2kimlqvjiGAJDSne6yiLcpiObt80gU";
+      setTimeout(function () {
+        var payload = `\n\ntitle : ${request.body.title}\nเรื่องราว : ${request.body.story}\nนามปากกา : ${request.body.author}\ninstagram : ${request.body.instagram}\n\nวันที่ : ${thaiDateTime}`;
+
+        const formData = new FormData();
+        formData.append("message", `${payload}`);
+        formData.append("access_token", line_api);
+        fetch("https://notify-api.line.me/api/notify", {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${line_api}`,
+            ...formData.getHeaders(),
+          },
+          body: formData,
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }, 8000);
+      response.redirect("/storygluay-submitted");
+    }
+  );
+});
+
+app.get("*", function (request, response) {
+  response.redirect("/storygluay");
+});
+
+app.listen(3000, function () {
+  console.log("Listening at Port 3000");
+});
